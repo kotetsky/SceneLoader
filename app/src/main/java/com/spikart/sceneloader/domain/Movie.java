31fea78622f5e39605da2488b40bbf4df93491fd
Aerol <kotetsky@gmail.com>
2019-08-11 11:47:47 +0300

    Movie item binded to room added

--- app/src/main/java/com/spikart/sceneloader/domain/Movie.java
+++ app/src/main/java/com/spikart/sceneloader/domain/Movie.java
@@ -1,6 +1,9 @@
 package com.spikart.sceneloader.domain;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -8,11 +11,188 @@ import androidx.room.PrimaryKey;
 import com.google.gson.annotations.SerializedName;
 
 
-@Entity(tableName="movies")
+@Entity(tableName = "movies")
 public class Movie extends BaseObservable {
 
-    @PrimaryKey() @ColumnInfo(name="id") @SerializedName(value="id")
+    @PrimaryKey()
+    @ColumnInfo(name = "id")
+    @SerializedName(value = "id")
     private Integer id;
 
+    @ColumnInfo(name = "vote_count")
+    @SerializedName(value = "vote_count")
+    private Integer mVoteCount;
 
+    @ColumnInfo(name = "video")
+    @SerializedName(value = "video")
+    private Boolean mVideo;
+
+    @ColumnInfo(name = "vote_average")
+    @SerializedName(value = "vote_average")
+    private Float mVoteAverage;
+
+    @ColumnInfo(name = "title")
+    @SerializedName(value = "title")
+    private String mTitle;
+
+    @ColumnInfo(name = "popularity")
+    @SerializedName("popularity")
+    private Float mPopularity;
+
+    @ColumnInfo(name = "poster_path")
+    @SerializedName(value = "poster_path")
+    private String mPosterPath;
+
+    @ColumnInfo(name = "original_language")
+    @SerializedName(value = "original_language")
+    private String mOriginalLanguage;
+
+    @ColumnInfo(name = "original_title")
+    @SerializedName(value = "original_title")
+    private String mOriginalTitle;
+
+    @ColumnInfo(name = "backdrop_path")
+    @SerializedName(value = "backdrop_path")
+    private String mBackdropPath;
+
+    @ColumnInfo(name = "adult")
+    @SerializedName(value = "adult")
+    private Boolean mAdult;
+
+    @ColumnInfo(name = "overview")
+    @SerializedName(value = "overview")
+    private String mOverview;
+
+    @ColumnInfo(name = "release_date")
+    @SerializedName(value = "release_date")
+    private String mReleaseDate;
+
+    public static DiffUtil.ItemCallback<Movie> DIFF_CALLBACK = new DiffUtil.ItemCallback<Movie>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Movie oldItem, @NonNull Movie newItem) {
+            return oldItem.getId().equals(newItem.getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Movie oldItem, @NonNull Movie newItem) {
+            return oldItem.getId().equals(newItem.getId());
+        }
+    };
+
+    @Bindable
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Bindable
+    public Integer getVoteCount() {
+        return mVoteCount;
+    }
+
+    public void setVoteCount(Integer mVoteCount) {
+        this.mVoteCount = mVoteCount;
+    }
+
+    @Bindable
+    public Boolean getVideo() {
+        return mVideo;
+    }
+
+    public void setVideo(Boolean mVideo) {
+        this.mVideo = mVideo;
+    }
+
+    @Bindable
+    public Float getVoteAverage() {
+        return mVoteAverage;
+    }
+
+    public void setVoteAverage(Float mVoteAverage) {
+        this.mVoteAverage = mVoteAverage;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    @Bindable
+    public Float getPopularity() {
+        return mPopularity;
+    }
+
+    public void setPopularity(Float mPopularity) {
+        this.mPopularity = mPopularity;
+    }
+
+    @Bindable
+    public String getPosterPath() {
+        return mPosterPath;
+    }
+
+    public void setPosterPath(String mPosterPath) {
+        this.mPosterPath = mPosterPath;
+    }
+
+    @Bindable
+    public String getOriginalLanguage() {
+        return mOriginalLanguage;
+    }
+
+    public void setOriginalLanguage(String mOriginalLanguage) {
+        this.mOriginalLanguage = mOriginalLanguage;
+    }
+
+    @Bindable
+    public String getOriginalTitle() {
+        return mOriginalTitle;
+    }
+
+    public void setOriginalTitle(String mOriginalTitle) {
+        this.mOriginalTitle = mOriginalTitle;
+    }
+
+    @Bindable
+    public String getBackdropPath() {
+        return mBackdropPath;
+    }
+
+    public void setBackdropPath(String mBackdropPath) {
+        this.mBackdropPath = mBackdropPath;
+    }
+
+    @Bindable
+    public Boolean getAdult() {
+        return mAdult;
+    }
+
+    public void setAdult(Boolean mAdult) {
+        this.mAdult = mAdult;
+    }
+
+    @Bindable
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public void setOverview(String mOverview) {
+        this.mOverview = mOverview;
+    }
+
+    @Bindable
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String mReleaseDate) {
+        this.mReleaseDate = mReleaseDate;
+    }
 }
